739b7e9bc065e421ee7c69f4c747e213c82daefa
ivanchikb360 <95337811+ivanchikb360@users.noreply.github.com>
2023-07-27 13:42:30 -0700

    Add files via upload

---
+++ piano/App.js
@@ -0,0 +1,69 @@
+function playC() {
+    var audio = document.getElementById("c");
+    audio.play();
+
+}
+
+function playCSharp() {
+    var audio = document.getElementById("cSharp");
+    audio.play();
+
+}
+function playD() {
+    var audio = document.getElementById("d");
+    audio.play();
+
+}
+
+function playDSharp() {
+    var audio = document.getElementById("dSharp");
+    audio.play();
+
+}
+
+function playE() {
+    var audio = document.getElementById("e");
+    audio.play();
+
+}
+function playF() {
+    var audio = document.getElementById("f");
+    audio.play();
+
+}
+
+function playFSharp() {
+    var audio = document.getElementById("fSharp");
+    audio.play();
+
+}
+
+function playG() {
+    var audio = document.getElementById("g");
+    audio.play();
+
+}
+
+function playGSharp() {
+    var audio = document.getElementById("gSharp");
+    audio.play();
+
+}
+
+function playA() {
+    var audio = document.getElementById("a");
+    audio.play();
+
+}
+
+function playASharp() {
+    var audio = document.getElementById("aSharp");
+    audio.play();
+
+}
+
+function playB() {
+    var audio = document.getElementById("b");
+    audio.play();
+
+}
